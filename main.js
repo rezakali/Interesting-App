2fff3fcd3430957cf75b8a7224041970bef45f38
rezakali <rezakali1993@gmail.com>
2022-06-11 01:04:47 +0530

    Minor chnages

--- main.js
+++ main.js
@@ -18,65 +18,61 @@ let quoteAuthor = document.getElementById("quoteauthor");
 let jokeAuthor = document.getElementById("jokeauthor");
 let quoteBtn = document.getElementById("quotebtn");
 let jokeBtn = document.getElementById("jokebtn");
-let cardBtns = document.querySelectorAll(".cardbtn")
+let cardBtns = document.querySelectorAll(".cardbtn");
 // let downloadLink = document.getElementById("link");
 
-dogBtn.addEventListener("click", getDog)
-catBtn.addEventListener("click", getCat)
-animalBtn.addEventListener("click", getAnimal)
-quoteBtn.addEventListener("click", getQuote)
-jokeBtn.addEventListener("click", getJoke)
+dogBtn.addEventListener("click", getDog);
+catBtn.addEventListener("click", getCat);
+animalBtn.addEventListener("click", getAnimal);
+quoteBtn.addEventListener("click", getQuote);
+jokeBtn.addEventListener("click", getJoke);
 
-
-function getDog(){
-    fetch('https://dog.ceo/api/breeds/image/random')
+function getDog() {
+  fetch("https://dog.ceo/api/breeds/image/random")
     .then((res) => res.json())
     .then((data) => {
-        dogHolder.src = data.message
-    })
+      dogHolder.src = data.message;
+    });
 }
 
-function getCat(){
-    fetch('https://api.thecatapi.com/v1/images/search')
+function getCat() {
+  fetch("https://api.thecatapi.com/v1/images/search")
     .then((res) => res.json())
     .then((data) => {
-        catHolder.src = data[0].url
-
-    })
+      catHolder.src = data[0].url;
+    });
 }
-function getAnimal(){
-    fetch('https://zoo-animal-api.herokuapp.com/animals/rand')
+function getAnimal() {
+  fetch("https://zoo-animal-api.herokuapp.com/animals/rand")
     .then((res) => res.json())
     .then((data) => {
-        animalHolder.src = data.image_link
-        animalName.innerHTML = data.name
-        animalType.innerHTML = data.animal_type
-        animalSpan.innerHTML = data.lifespan
-        animalHabitat.innerHTML = data.habitat
-        animalLocation.innerHTML = data.geo_range
+      animalHolder.src = data.image_link;
+      animalName.innerHTML = data.name;
+      animalType.innerHTML = data.animal_type;
+      animalSpan.innerHTML = data.lifespan;
+      animalHabitat.innerHTML = data.habitat;
+      animalLocation.innerHTML = data.geo_range;
     })
-    .catch((err) => console.log(err))
+    .catch((err) => console.log(err));
 }
-function getQuote(){
-    fetch('http://api.quotable.io/random')
+function getQuote() {
+  fetch("https://api.quotable.io/random")
     .then((res) => res.json())
     .then((data) => {
-        quote.innerHTML = data.content
-        quoteAuthor.innerHTML = data.author
+      quote.innerHTML = data.content;
+      quoteAuthor.innerHTML = data.author;
     })
-    .catch((err) => console.log(err))
+    .catch((err) => console.log(err));
 }
-function getJoke(){
-    fetch('https://v2.jokeapi.dev/joke/Any?safe-mode')
+function getJoke() {
+  fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
     .then((res) => res.json())
     .then((data) => {
-        joke.innerHTML = data.setup
-        jokeAuthor.innerHTML = data.delivery
+      joke.innerHTML = data.setup;
+      jokeAuthor.innerHTML = data.delivery;
     })
-    .catch((err) => console.log(err))
+    .catch((err) => console.log(err));
 }
-
-
 
 getCat();
 getDog();
@@ -84,31 +80,29 @@ getAnimal();
 getQuote();
 getJoke();
 
-
-for(let i=0; i < cardBtns.length; i++ ){
-    cardBtns[0].addEventListener("click", () => {
-        cardBtns[0].classList.add("active")
-        cardBtns[1].classList.remove("active")
-        cardBtns[2].classList.remove("active")
-        card2.classList.remove("active")
-        card3.classList.remove("active")
-        card1.classList.add("active")
-    })
-    cardBtns[1].addEventListener("click", () => {
-        cardBtns[1].classList.add("active")
-        cardBtns[0].classList.remove("active")
-        cardBtns[2].classList.remove("active")
-        card1.classList.remove("active")
-        card3.classList.remove("active")
-        card2.classList.add("active")
-    })
-    cardBtns[2].addEventListener("click", () => {
-        cardBtns[2].classList.add("active")
-        cardBtns[1].classList.remove("active")
-        cardBtns[0].classList.remove("active")
-        card1.classList.remove("active")
-        card2.classList.remove("active")
-        card3.classList.add("active")
-    })
+for (let i = 0; i < cardBtns.length; i++) {
+  cardBtns[0].addEventListener("click", () => {
+    cardBtns[0].classList.add("active");
+    cardBtns[1].classList.remove("active");
+    cardBtns[2].classList.remove("active");
+    card2.classList.remove("active");
+    card3.classList.remove("active");
+    card1.classList.add("active");
+  });
+  cardBtns[1].addEventListener("click", () => {
+    cardBtns[1].classList.add("active");
+    cardBtns[0].classList.remove("active");
+    cardBtns[2].classList.remove("active");
+    card1.classList.remove("active");
+    card3.classList.remove("active");
+    card2.classList.add("active");
+  });
+  cardBtns[2].addEventListener("click", () => {
+    cardBtns[2].classList.add("active");
+    cardBtns[1].classList.remove("active");
+    cardBtns[0].classList.remove("active");
+    card1.classList.remove("active");
+    card2.classList.remove("active");
+    card3.classList.add("active");
+  });
 }
-
